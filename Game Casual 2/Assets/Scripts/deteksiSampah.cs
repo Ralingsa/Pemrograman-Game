0818af2d3c105820bde2c251dde4a897ddbceaa2
ranggaLP <ranggaghtdghtd@gmail.com>
2021-09-10 17:06:03 +0700

    9/10/2021

---
+++ Game Casual 2/Assets/Scripts/deteksiSampah.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deteksiSampah : MonoBehaviour
+{
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+    private AudioSource mediaPlayerBenar;
+    private AudioSource mediaPlayerSalah;
+    public Text textScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        mediaPlayerBenar.clip = audioBenar;
+
+        mediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        mediaPlayerSalah.clip = audioSalah;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(nameTag))
+        {
+            data.score += 25;
+            textScore.text = data.score.ToString();
+            Destroy(collision.gameObject);
+            mediaPlayerBenar.Play();
+        }
+        else
+        {
+            data.score -= 5;
+            textScore.text = data.score.ToString();
+            Destroy(collision.gameObject);
+            mediaPlayerSalah.Play();
+        }
+    }
+}
---
+++ Game Casual 2/Assets/Scripts/gameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour
+{
+    float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >2)
+        {
+            data.score = 0;
+            SceneManager.LoadScene("gameplay");
+        }
+    }
+}
